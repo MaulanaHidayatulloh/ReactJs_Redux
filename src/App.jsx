53605db26f1df3271d7e822adcb9595e7e1c8eb3
MaulanaHidayatulloh <ulyadbaim@gmail.com>
2024-05-13 18:45:00 +0700

    pertama

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CounterComponent from "../redux/counterComponent";
+import UserComponent from "../redux/userComponent";
+
+function App() {
+  return (
+    <div>
+      <CounterComponent />
+      <UserComponent />
+    </div>
+  );
+}
+
+export default App;
